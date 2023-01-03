2b0b8931e50221bdcf5280ab1a9d4eaa3c6a806b
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2023-01-03 14:52:09 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [userQuery, setUserQuery] = useState("");
+
+  const handleSearch = () => {
+    window.open(`https://google.com/search?q=${userQuery}`, "_blank");
+  };
+  const handleUserQuery = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setUserQuery(value);
+  };
+
+  function handleKeyPress(e) {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  }
+
   return (
     <div className="App">
-
+      <h1>Hello Vijay</h1>
+      <div className="form">
+        <input
+          type="text"
+          value={userQuery}
+          onChange={handleUserQuery}
+          onKeyPress={handleKeyPress}
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
     </div>
   );
 }
